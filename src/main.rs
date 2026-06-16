e0f5bf66bc2e109f2de61f29477a66eb1f86a797
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-06-16 20:48:13 +0200

    rwlock sandbox

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod palidrome_number;
 mod search_insert_position;
 mod two_sum;
 mod valid_sudoku;
+mod count_and_say;
 
 fn main() {
     // two_sum::test2();
@@ -11,5 +12,8 @@ fn main() {
     // palidrome_number::test1();
     // longest_common_prefix::test1();
     // valid_sudoku::test1();
-    search_insert_position::test1();
+    // search_insert_position::test1();
+    // count_and_say::test1();
+
+    count_and_say::s_test();
 }
--- src/tokio_sandbox.rs
+++ src/tokio_sandbox.rs
@@ -2,11 +2,15 @@ use rand::{self, RngExt};
 use std::time::Duration;
 use tokio::task;
 use tokio::time;
+use tokio::sync::RwLock;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
-    spawn_example().await;
+    // spawn_example().await;
+    random_write_example().await;
 }
+
 
 async fn spawn_example() {
     let join_handler = tokio::task::spawn(random_load(Duration::new(5, 0)));
@@ -28,4 +32,26 @@ async fn random_load(sleep_time: Duration) {
     tokio::time::sleep(sleep_time).await;
     println!("Sleep of task: {task_id:?} end");
     println!("some_random: {some_random:?}");
+}
+
+
+async fn random_write_example(){
+    let mut shared_vec: Vec<u32> = vec![1,2,3];
+    let sv_lock = Arc::new(RwLock::new(shared_vec));
+
+    let jh1 = tokio::task::spawn(
+        random_write(sv_lock.clone())
+    );
+
+    jh1.await.unwrap();
+}
+
+async fn random_write(rwlock: Arc<RwLock<Vec<u32>>>) {
+    let task_id = tokio::task::id();
+    let mut rng: rand::rngs::SmallRng = rand::make_rng();
+    let some_random: u32 = rng.random();
+    // let resource = rwlock.read().await; // WARNING: If you do not put it in a scope then you'll have a deadlock!;
+    let mut w_resource = rwlock.write().await;
+    w_resource.push(6);
+    println!("Resource from task: {task_id}, {w_resource:?}");
 }
